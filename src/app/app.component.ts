e130bbe3a4e8e1ae46b6e12534012e9adae9765c
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-10-22 23:12:13 -0300

    uso de ngFor con un array de objetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,15 @@ export class AppComponent {
   title = 'platzi-store';
 
   items = ['tonton', 'sultan', 'rabito', 'robert', 'tatin', 'pelusa'];
+
+  products: Product[] = [
+    { id: '1', image: 'assets/images/camisetas.png', title: 'Camiseta', price: 500, description: 'very good' },
+    { id: '2', image: 'assets/images/hoodie.png', title: 'Hoodie', price: 600, description: 'very good' },
+    { id: '3', image: 'assets/images/mug.png', title: 'Mug', price: 300, description: 'very good' },
+    { id: '4', image: 'assets/images/pin.png', title: 'Pin', price: 400, description: 'very good' },
+    { id: '5', image: 'assets/images/stickers1.png', title: 'Stickers1', price: 100, description: 'very good' },
+    { id: '6', image: 'assets/images/stickers2.png', title: 'Stickers2',  price: 200, description: 'very good' }
+  ];
 
   addItem() {
     this.items.push('otro perro');
