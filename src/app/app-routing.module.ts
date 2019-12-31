4250d10ff59786d097affe247403520f4f253d9f
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-12-31 20:06:11 -0300

    se agrega guard seguridad en rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProductsComponent } from './products/products.component';
 import { DemoComponent } from './demo/demo.component';
@@ -9,16 +8,18 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
 
+import { AdminGuard } from './admin.guard';
+
 const routes: Routes = [
   {  
     path: '',
     component: LayoutComponent,
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
+      { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
       { path: 'products', component: ProductsComponent },
       { path: 'products/:id', component: ProductDetailComponent },
-      { path: 'contact', component: ContactComponent },
+      { path: 'contact', canActivate: [AdminGuard] , component: ContactComponent },
     ]
   },
   { path: 'demo', component: DemoComponent },
@@ -26,7 +27,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { BannerComponent } from './components/banner/banner.component';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './components/home/home.component';
 import { HomeRoutingModule } from './home-reouting.module';
 
 @NgModule({
@@ -9,6 +11,7 @@ import { HomeRoutingModule } from './home-reouting.module';
         HomeComponent
     ],
     imports: [
+        CommonModule,
         HomeRoutingModule
     ]
 })
