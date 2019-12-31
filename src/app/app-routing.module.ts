2fb3f6b8a69faf512cfec07afcc6fd4d37c9315f
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-12-31 11:33:14 -0300

    se agregan routas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,17 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProductsComponent } from './products/products.component';
 import { DemoComponent } from './demo/demo.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'demo', component: DemoComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
