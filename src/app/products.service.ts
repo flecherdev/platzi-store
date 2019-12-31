f6b4f12d0a055cb6a4a0141bcaf03ea392b4942f
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-12-31 11:31:47 -0300

    se agrega servicio de products

---
+++ src/app/products.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  products: Product[] = [
+    { id: '1', image: 'assets/images/camiseta.png', title: 'Camiseta', price: 500, description: 'very good' },
+    { id: '2', image: 'assets/images/hoodie.png', title: 'Hoodie', price: 600, description: 'very good' },
+    { id: '3', image: 'assets/images/mug.png', title: 'Mug', price: 300, description: 'very good' },
+    { id: '4', image: 'assets/images/pin.png', title: 'Pin', price: 400, description: 'very good' },
+    { id: '5', image: 'assets/images/stickers1.png', title: 'Stickers1', price: 100, description: 'very good' },
+    { id: '6', image: 'assets/images/stickers2.png', title: 'Stickers2',  price: 200, description: 'very good' }
+  ];
+
+  constructor() { }
+
+  getAllProducts() {
+    return this.products;
+  }
+
+  getProducts(id) {
+    return this.products.find(item => id === item.id);
+  }
+}
