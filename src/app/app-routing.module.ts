670a9bf7d7e9c9e99f3ac6ea488faba88a8d422d
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-12-31 18:23:58 -0300

    se agrega layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,20 @@ import { ProductsComponent } from './products/products.component';
 import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent },
-  { path: 'products', component: ProductsComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
-  { path: 'contact', component: ContactComponent },
+  {  
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'contact', component: ContactComponent },
+    ]
+  },
   { path: 'demo', component: DemoComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
