cc3d20fc414e7c248913d8bf0e4e1eed9e085763
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-01-13 23:07:57 -0300

    se agrega boton y logica para update,delete y create

--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { ProductsService } from '../../../core/services/products/products.service';
-import { Product } from '../../../product.model';
+import { Product } from '../../../core/models/product.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -20,9 +20,43 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       const id = params.id;
-      this.product = this.productService.getProducts(id);
-      console.log(this.product);
+      console.log(id);
+      this.fetchProduct(id);
     });
   }
 
+  fetchProduct(id: string) {
+    this.productService.getProducts(id).subscribe(product => {
+      this.product = product;
+    });
+  }
+
+  createProduct() {
+    const newProduct: Product = {
+      id: '7',
+      title: 'nuevo',
+      image: 'assets/images/camiseta.png',
+      price: 50,
+      description: 'nuevo producto'
+    };
+    this.productService.createProduct(newProduct).subscribe(product => {
+      console.log(product);
+    });
+  }
+
+  updateProduct() {
+    const updateProduct: Partial<Product> = {
+      price: 5000,
+      description: 'update producto'
+    };
+    this.productService.updateProduct('5e1b935e0a2c4bdb936ec7c7', updateProduct).subscribe(product => {
+      console.log(product);
+    });
+  }
+
+  deleteProduct() {
+    this.productService.deleteProduct(this.product._id).subscribe(product => {
+      console.log(product);
+    });
+  }
 }
--- src/app/product/components/products/products.component.ts
+++ src/app/product/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../../../product.model';
+import { ProductsService } from '../../../core/services/products/products.service';
+import { Product } from '../../../core/models/product.model';
 
 @Component({
   selector: 'app-products',
@@ -8,22 +9,26 @@ import { Product } from '../../../product.model';
 })
 export class ProductsComponent implements OnInit {
 
-  products: Product[] = [
-    { id: '1', image: 'assets/images/camiseta.png', title: 'Camiseta', price: 500, description: 'very good' },
-    { id: '2', image: 'assets/images/hoodie.png', title: 'Hoodie', price: 600, description: 'very good' },
-    { id: '3', image: 'assets/images/mug.png', title: 'Mug', price: 300, description: 'very good' },
-    { id: '4', image: 'assets/images/pin.png', title: 'Pin', price: 400, description: 'very good' },
-    { id: '5', image: 'assets/images/stickers1.png', title: 'Stickers1', price: 100, description: 'very good' },
-    { id: '6', image: 'assets/images/stickers2.png', title: 'Stickers2',  price: 200, description: 'very good' }
-  ];
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit() {
+    this.fetchProducts();
   }
 
   clickProduct(id: number) {
     console.log(id);
   }
+
+  fetchProducts() {
+    this.productsService.getAllProducts().subscribe(products => {
+      console.log(products);
+      this.products = products;
+    });
+  }
+
 
 }
